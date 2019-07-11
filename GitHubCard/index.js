84835969e28764ffffdf488bc598c07af6838276
jose restrepo <arturo.restrepo@gmail.com>
2019-07-11 15:15:09 -0500

    Promise you it is in progress

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,16 @@
            https://api.github.com/users/<your name>
 */
 
+const promise = axios.get('https://api.github.com/users/hamidoudiallo96');
+promise
+.then(item =>{
+  console.log(item);
+})
+.catch(item =>{
+  console.log('ERROR: Try Again Dude !');
+});
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
